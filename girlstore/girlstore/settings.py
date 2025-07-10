1f2f06304d9f619cff73faf1a0c0cf545428513e
Khushbusaifi012 <khushbusaifi27@gmail.com>
2025-07-10 12:51:53 +0530

    make website responsive and fixed login and signup buttons

--- girlstore/girlstore/settings.py
+++ girlstore/girlstore/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this for static files
 ]
 
 ROOT_URLCONF = 'girlstore.urls'
